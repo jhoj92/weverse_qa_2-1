fae7386561e11cc958dedd67c5612319f3361f02
jhoj92 <jhoj92@gmail.com>
2026-05-19 15:52:37 +0900

    과제 2-1 제출

--- test_weverse_signup.py
+++ test_weverse_signup.py
@@ -7,7 +7,7 @@ load_dotenv()
 def test_weverse_signup_and_extract_wid(page):
     
     extracted_wid = None
-
+    
     # WID 데이터 확인 함수
     def handle_response(response):
         nonlocal extracted_wid
@@ -21,13 +21,10 @@ def test_weverse_signup_and_extract_wid(page):
 
     page.on("response", handle_response)
 
-    # ==================================================
-    # 테스트 시작
-    # ==================================================
     signup_page = WeverseSignupPage(page)
     print("테스트 시작: Weverse 회원가입 및 WID 데이터 추출")
 
-    # 테스트 데이터 불러오기
+    # 0. 테스트 데이터 불러오기
     test_id = os.getenv("TEST_ID")
     test_pw = os.getenv("TEST_PW")
     test_nickname = os.getenv("TEST_NICKNAME")
@@ -36,18 +33,15 @@ def test_weverse_signup_and_extract_wid(page):
     signup_page.go_to_email_signup()
     
     signup_page.submit_email_for_verification(test_id)
-    print("인증코드 수동 입력 대기 중...")
+    print("\n1. 인증코드 수동 입력 대기 중...")
     signup_page.wait_for_manual_verification()
 
     signup_page.fill_account_details(test_pw, test_nickname)
     signup_page.complete_signup()
     
     page.wait_for_timeout(3000) # WID 데이터 응답 대기
-    print("WID 데이터 추출 대기 중...")
+    print("2. WID 데이터 추출 대기 중...")
 
-    # ==================================================
-    # 결과 출력
-    # ==================================================
     print("\n==============================")
     print("[과제 2-1 테스트 결과]")
     print(f"ID : {test_id}")
